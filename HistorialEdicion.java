0522bcde388775fd039827896203ff14d2a5dd7b
Sebas$ <sebaslopez2106@gmail.com>
2026-08-23 13:59:20 -0300

    add pilaRehacer methods

--- HistorialEdicion.java
+++ HistorialEdicion.java
@@ -1,15 +1,35 @@
 public class HistorialEdicion {
     private BufferGap<Character> buffer;    // instancia de BufferGap sobre el cual realizar el historial
-    private PilaES<Comando> pilaCtrlZ;      // pila para Ctrl-Z
-    private PilaES<Comando> pilaCtrlY;      // pila para Ctrl-y
+    private PilaES<Comando> pilaDeshacer;      // pila para deshacer (Ctrl-z)
+    private PilaES<Comando> pilaRehacer;      // pila para rehacer (Ctrl-y)
 
 
     public HistorialEdicion (BufferGap<Character> bf) {
         buffer = bf;
-        pilaCtrlZ = new PilaES<Comando>();
-        pilaCtrlY = new PilaES<Comando>();
+        pilaDeshacer = new PilaES<Comando>();
+        pilaRehacer = new PilaES<Comando>();
     } // <-> end HistorialEdicion constructor
 
 
+
+    public boolean rehacer () {
+        /*
+        *Vuelve a ejecutar el último comando deshecho y lo devuelve a la pila de deshacer.
+        * Retorna false si no hay nada que rehacer.
+        */
+        if (pilaRehacer.size() > 0) {
+            pilaRehacer.desapilar().ejecutar();
+            return true;
+        }
+
+        return false;
+    } // <-> end rehacer method
+
+
+
+    public int sizeRehacer () {
+        /* retorna la cantidad de elementos que contiene pilaCtrlY */
+        return pilaRehacer.size();
+    } // <-> end sizeRehacer method
 
 } // <> end HistorialEdicion class
